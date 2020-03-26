4c0bd7f71316b1281351a9d952ab173409e691f0
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-03-27 04:20:58 +0430

    Create execute_playbook.py

---
+++ execute_playbook.py
@@ -0,0 +1,9 @@
+import  os
+def playbook():
+    os.system(' apt update ; apt install software-properties-common -y ;  apt-add-repository --yes --update ppa:ansible/ansible && apt install ansible -y ')
+    os.system('sudo add-apt-repository ppa:deadsnakes/ppa ; apt install python3.7 -y && python3.7 --version')
+    os.system(' apt install python3-pip -y && pip3 install jinja2 configparser ')
+    os.system('ansible-playbook -vvv  --flush-cache --list-tasks --become-user root playbook/playbook-install-etcd.yml ')
+    os.system('ansible-playbook -vvv  --flush-cache --list-tasks --become-user root playbook/playbook-install-master.yml ')
+    os.system('ansible-playbook -vvv  --flush-cache --list-tasks --become-user root playbook/playbook-install-worker.yml ')
+playbook()
