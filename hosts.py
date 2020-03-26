c33648e436d652ef9d3186dd65d53c0ffb0e3cf9
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-03-27 04:22:01 +0430

    Update hosts.py

--- hosts.py
+++ hosts.py
@@ -1,41 +1,71 @@
-import logging,colorlog,os,stat,time
+#!/usr/bin/python3.6
+from config import *
+import logging , os , time,colorlog
 colorlog.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S')
-import configparser , os
-conf = configparser.ConfigParser()
-def write_conf():
-    conf.add_section('ETCD')
-    conf.set('ETCD','nodes','127.0.0.1,127.0.0.2')
-    conf.add_section('MASTER-NODES')
-    conf.set('MASTER-NODES','nodes','127.0.0.3,127.0.0.4')
-    conf.add_section('WORKER-NODES')
-    conf.set('WORKER-NODES','nodes','127.0.0.5,127.0.0.6')
-    conf.add_section('HAPROXY-NODES')
-    conf.set('HAPROXY-NODES','nodes','127.0.0.7,127.0.0.8')
-    conf.add_section('KEEPALIVED')
-    conf.set('KEEPALIVED','ip','127.0.0.10')
-    conf.add_section('PODSUBNET')
-    conf.set('PODSUBNET','podsubnet','30.30.1.0/16')
-    with open('kube.cfg','w') as configfile:
-        conf.write(configfile)
-def read_conf():
-      conf.read('kube.cfg')
-      global etcd
-      global masternodes
-      global workernodes
-      global haproxynodes
-      global keepalivedip
-      etcd = str(conf.get('ETCD', 'nodes')).replace(' ', '').split(',')
-      masternodes = str(conf.get('MASTER-NODES', 'nodes')).replace(' ', '').split(',')
-      workernodes = str(conf.get('WORKER-NODES', 'nodes')).replace(' ', '').split(',')
-      haproxynodes = str(conf.get('HAPROXY-NODES', 'nodes')).replace(' ', '').split(',')
-      podsubnet = conf.get('PODSUBNET','podsubnet')
-      keepalivedip = str(conf.get('KEEPALIVED','ip'))
-if not os.path.exists('kube.cfg'):
-    logging.info('  create kube.cfg \n')
-    write_conf()
-read_conf()
-path=('kube.cfg')
-a=os.stat(path)
-b=time.ctime(a[stat.ST_MTIME])
-print(b)
+def hosts():
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    if os.path.exists("tmp/hosts"):
+        os.remove("tmp/hosts")
+    hfile = open('tmp/hosts', 'a+')
+    hfile_check = open('/etc/hosts', 'r')
+    for line in hfile_check:
+            if  '############Added by script#############' in line:
+                logging.warn('\n/etc/hosts  \nplease delete this line   \n'+ line +' \nand afterwards\n')
+
+                exit(1)
+    hfile.write('\n############Added by script#############\n')
+    for lenetcd in range(len(etcd)):
+        hfile.write('\n{0}   etcd-{1} '.format(etcd[lenetcd], lenetcd))
+    for lenmasternodes in range(len(masternodes)):
+        hfile.write('\n{0}   k8sm-{1} '.format(masternodes[lenmasternodes], lenmasternodes))
+    for lenworkernodes in range(len(workernodes)):
+        hfile.write('\n{0}   k8sw-{1} '.format(workernodes[lenworkernodes], lenworkernodes))
+    for lenhaproxynodes in range(len(haproxynodes)):
+            hfile.write('\n{0}   haproxy-{1} '.format(haproxynodes[lenhaproxynodes], lenhaproxynodes))
+    hfile.write('\n{0}   k8s-haproxy '.format(keepalivedip))
+
+    hfile.close()
+
+    ###send tmp/hosts
+    for lenetcd in range(len(etcd)):
+            time.sleep(1)
+            if  (
+                    os.system(('scp tmp/hosts root@{0}:/tmp/'.format(etcd[lenetcd]))) == 0
+                    and os.system("ssh root@{0} 'cat /tmp/hosts >> /etc/hosts'".format(etcd[lenetcd])) == 0
+
+            ):
+                    logging.info('send tmp/hosts to {0} :/tmp/hosts \n'.format(etcd[lenetcd]))
+            else:
+                    logging.error('NOT send tmp/hosts to {0} :/tmp/hosts \n'.format(etcd[lenetcd]))
+                    break
+    for lenmasternodes in range(len(masternodes)):
+            time.sleep(1)
+            if (
+                  os.system("ssh root@{0} 'cat /tmp/hosts >> /etc/hosts'".format(masternodes[lenmasternodes])) == 0
+                  and os.system('scp tmp/hosts root@{0}:/tmp/'.format(masternodes[lenmasternodes])) == 0
+             ):
+                  logging.info('send tmp/hosts to {0} :/tmp/hosts \n'.format(masternodes[lenmasternodes]))
+            else:
+                  logging.error('NOT send tmp/hosts to {0} :/tmp/hosts \n'.format(masternodes[lenmasternodes]))
+                  break
+    for lenworkernodes in range(len(workernodes)):
+            if (
+                 os.system("ssh root@{0} 'cat /tmp/hosts >> /etc/hosts'".format(workernodes[lenworkernodes])) == 0
+                 and os.system('scp tmp/hosts root@{0}:/tmp/'.format(workernodes[lenworkernodes])) == 0
+            ):
+                logging.info('send tmp/hosts to {0} :/tmp/hosts \n'.format(workernodes[lenworkernodes]))
+            else:
+                   logging.error('NOT send tmp/hosts to {0} :/tmp/hosts \n'.format(workernodes[lenworkernodes]))
+                   break
+    for lenhaproxynodes in range(len(haproxynodes)):
+          if (
+                  os.system("ssh root@{0} 'cat /tmp/hosts >> /etc/hosts'".format(haproxynodes[lenhaproxynodes])) == 0
+                  and os.system('scp tmp/hosts root@{0}:/tmp/'.format(haproxynodes[lenhaproxynodes])) == 0
+             ):
+              logging.info('send tmp/hosts to {0} :/tmp/hosts \n'.format(haproxynodes[lenhaproxynodes]))
+          else:
+              logging.error('NOT send tmp/hosts to {0} :/tmp/hosts \n'.format(haproxynodes[lenhaproxynodes]))
+              break
+hosts()
