c945f891c5dc9f01233ccef252b1263541714ddf
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-03-28 04:11:01 +0430

    Update execute_playbook.py

--- execute_playbook.py
+++ execute_playbook.py
@@ -3,46 +3,46 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(mes
 def playbook():
      os.system(' apt update ; apt install software-properties-common -y ;  apt-add-repository --yes --update ppa:ansible/ansible && apt install ansible -y ')
 
-     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-swapoff.yml '):
+     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-swapoff.yml -i ansible/hosts'):
          logging.info('ansible-playbook -vvv  --flush-cache   playbook/playbook-swapoff.yml')
      else:
         logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-swapoff.yml')
         exit(1)
-     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-disable-swap-fstab.yml '):
+     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-disable-swap-fstab.yml -i ansible/hosts'):
         logging.info('ansible-playbook -vvv  --flush-cache   playbook/playbook-disable-swap-fstab.yml')
      else:
          logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-disable-swap-fstab.yml')
          exit(1)
-     if os.system(' apt install python3-pip -y && pip3 install jinja2 configparser '):
+     if os.system(' apt install python3-pip -y && pip3 install jinja2 configparser -i ansible/hosts'):
         logging.info('apt install python3-pip -y && pip3 install jinja2 configparser')
      else:
         logging.error('apt install python3-pip -y && pip3 install jinja2 configparser')
         exit(1)
-     if os.system('ansible-playbook -vvv  --flush-cache playbook/playbook-install-etcd.yml '):
+     if os.system('ansible-playbook -vvv  --flush-cache playbook/playbook-install-etcd.yml -i ansible/hosts'):
         logging.info('ansible-playbook -vvv  --flush-cache playbook/playbook-install-etcd.yml ')
 
      else:
         logging.error('ansible-playbook -vvv  --flush-cache playbook/playbook-install-etcd.yml ')
         exit(1)
-     if   os.system('ansible-playbook -vvv  --flush-cache  playbook/playbook-install-master.yml '):
+     if   os.system('ansible-playbook -vvv  --flush-cache  playbook/playbook-install-master.yml -i ansible/hosts'):
          logging.info('ansible-playbook -vvv  --flush-cache  playbook/playbook-install-master.yml ')
 
      else:
          logging.error('ansible-playbook -vvv  --flush-cache  playbook/playbook-install-master.yml ')
          exit(1)
 
-     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml '):
+     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml -i ansible/hosts'):
          logging.info('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml')
 
      else:
-         logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml')
+         logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml ')
          exit(1)
 
-     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml '):
-         logging.info('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml')
+     if os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml -i ansible/hosts'):
+         logging.info('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml ')
 
      else:
-         logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml')
+         logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml ')
          exit(1)
 
 playbook()
