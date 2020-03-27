ea2edeaebeaf0fadf2ad6ba415a45ea1e3423957
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-03-28 03:56:13 +0430

    Update execute_playbook.py

--- execute_playbook.py
+++ execute_playbook.py
@@ -1,10 +1,4 @@
-import  os
-def playbook():
-    os.system(' apt update ; apt install software-properties-common -y ;  apt-add-repository --yes --update ppa:ansible/ansible && apt install ansible -y ')
-    os.system('sudo add-apt-repository ppa:deadsnakes/ppa ; apt install python3.7 -y && python3.7 --version')
-    os.system(' apt install python3-pip -y && pip3 install jinja2 configparser ')
-    os.system('ansible-playbook -vvv  --flush-cache playbook/playbook-install-etcd.yml ')
-    os.system('ansible-playbook -vvv  --flush-cache  playbook/playbook-install-master.yml ')import  os,logging,colorlog
+import  os,logging,colorlog
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S')
 def playbook():
      os.system(' apt update ; apt install software-properties-common -y ;  apt-add-repository --yes --update ppa:ansible/ansible && apt install ansible -y ')
@@ -51,6 +45,4 @@ def playbook():
          logging.error('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-haproxy.yml')
          exit(1)
 
-playbook()
-    os.system('ansible-playbook -vvv  --flush-cache   playbook/playbook-install-worker.yml ')
 playbook()
